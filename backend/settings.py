ef76732c6f72e68f432c0b777d652d1100331ad7
Harshal <harshalnerkar11@gmail.com>
2026-07-26 08:13:16 +0000

    Removed hardcoded keys and unused files

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-onl3tc6whzoa5!lh3rq!+h#t#x%&%c$6*odkk%nouw=v@021'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
